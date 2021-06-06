662c20ccd1061693b66f13202f9274b57e5b750f
Elaine Cecília Gatto <elainececiliagatto@gmail.com>
2021-06-06 14:34:32 -0300

    Add files via upload

--- scripts/bestClus.R
+++ scripts/bestClus.R
@@ -203,7 +203,7 @@ cat("\n Copy Results to google drive")
 origem = paste(diretorios$folderDatasetResults, "/", dataset_name, "-results-bpc.tar.gz", sep="")
 destino = paste("cloud:elaine/[2021]ResultadosExperimentos/Best-Partition-Clus/", dataset_name, sep="")
 comando1 = paste("rclone -v copy ", origem, " ", destino, sep="")
-#print(system(comando1))
+print(system(comando1))
 
 
 ########################################################################################################################
@@ -211,7 +211,7 @@ cat("\n Copy Outupt to google drive")
 origem = diretorios$folderOutputDataset
 destino = paste("cloud:elaine/[2021]ResultadosExperimentos/Best-Partition-Clus/", dataset_name, sep="")
 comando2 = paste("rclone -v copy ", origem, " ", destino, sep="")
-#print(system(comando2))
+print(system(comando2))
 
 
 ##################################################################################################
